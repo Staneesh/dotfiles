806fcb40d99f9e863d2ed38e12ab6d31499c9248
Staneesh <blkreasons@outlook.com>
2023-12-22 22:01:07 +0100

    More `gnome` options

--- graphics.nix
+++ graphics.nix
@@ -22,4 +22,38 @@
       '';
     };
   };
+
+  # Use `dconf watch /` to track stateful changes you are doing, then set them here.
+  dconf.settings = {
+    # ...
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "org.gnome.Nautilus.desktop"
+        "alacritty.desktop"
+        "code.desktop"
+        "spotify.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      jdinhlife.gruvbox
+      yzhang.markdown-all-in-one
+      rust-lang.rust-analyzer
+      eamodio.gitlens
+      oderwat.indent-rainbow
+      vadimcn.vscode-lldb
+      serayuzgur.crates
+    ];
+  };
+
+  home.sessionVariables.GTK_THEME = "palenight";
 }
